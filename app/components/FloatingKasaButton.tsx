a88b994fa7c14f7e8fb3cd2dbf99a96a0dc78243
Joel Goldberger <yoelag@objectif.solutions>
2025-11-13 13:30:13 -0500

    Remove unused import

--- app/components/FloatingKasaButton.tsx
+++ app/components/FloatingKasaButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline'
 import AIChatModal from './AIChatModal'
 
