31ca2f3417fbb76bf459de4a185b4c06e8d00235
Joel Goldberger <yoelag@objectif.solutions>
2025-11-25 15:21:01 -0500

    Add internal state to ConfirmationDialog to prevent premature closes - ignore isOpen=false for 10 seconds

--- app/components/ConfirmationDialog.tsx
+++ app/components/ConfirmationDialog.tsx
@@ -141,7 +141,7 @@ export default function ConfirmationDialog({
 
   return (
     <Modal 
-      isOpen={isOpen} 
+      isOpen={effectiveIsOpen} 
       onClose={handleClose} 
       title="" 
       showCloseButton={false}
